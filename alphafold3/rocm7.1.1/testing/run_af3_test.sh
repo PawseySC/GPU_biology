3443f5864f6367273fbd5c68545cc22db0e880bc
SarahBeecroft <Sarah.beecroft@pawsey.org.au>
2026-08-17 16:48:35 +0800

    Update run_af3_test.sh to use new container image

--- alphafold3/rocm7.1.1/testing/run_af3_test.sh
+++ alphafold3/rocm7.1.1/testing/run_af3_test.sh
@@ -8,10 +8,13 @@
 #SBATCH --array=2-13
 
 module load singularity/3.11.4-nompi 
+singularity pull docker://quay.io/pawsey/alphafold3:rocm7.1.1
+
 MODEL_DIR=/path/to/model/weights
+CONTAINER=alphafold3_rocm7.1.1.sif
 
 srun -N 1 -n 1 -c 8 --gres=gpu:1 --gpus-per-task=1 \
-singularity exec alphafold3_claude.sif \
+singularity exec ${CONTAINER} \
       python3 /app/alphafold3/run_alphafold.py \
       --model_dir=${MODEL_DIR} \
       --json_path=2pv7_data_${SLURM_ARRAY_TASK_ID}mer.json \
